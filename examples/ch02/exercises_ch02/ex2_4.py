c8cdc44c9069f9cd113c32c887488fc76e70b7be
Edson Gomes <73556396+edson-gomes@users.noreply.github.com>
2021-12-06 16:45:52 -0300

    Added exercises 2.1-2.4
    Added and solved exercises 2.1-2.4, from chapter 2.

---
+++ examples/ch02/exercises_ch02/ex2_4.py
@@ -0,0 +1,11 @@
+# (Arithmetic) For each of the arithmetic operators +, -, *, /, // and **, display the value of an expression with 27.5 as the left operand and 2 as the right operand.
+
+left_operand = 27.5
+right_operand = 2
+
+print('27.5 + 2 =', (27.5 + 2))
+print('27.5 - 2 =', (27.5 - 2))
+print('27.5 * 2 =', (27.5 * 2))
+print('27.5 / 2 =', (27.5 / 2))
+print('27.5 // 2 =', (27.5 // 2))
+print('27.5 ** 2 =', (27.5 ** 2))
